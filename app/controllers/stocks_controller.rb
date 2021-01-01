f289a8dbbcdb3c0106f9d91df2a14c6fce2f9321
Artur Mrozinski <amrozinski@gmail.com>
2021-01-01 18:04:08 +0000

    app now lets used now if request wasnt valid.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    if params[:stock].present? // has it not been left blank??
+      @stock = Stock.new_lookup(params[:stock])
+        if @stock // if stock.rb did not return nil.
+          render 'users/my_portfolio'
+        else
+          flash[:alert] = "Please enter a valid symbol to search"
+          redirect_to my_portfolio_path
+        end
+    else
+        flash[:alert] = "This field canot be left blank"
+        redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,16 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: ENV['iex_api'],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
-  end
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
+   end
 end
+
+// client is defined to get info from IEX based on an API key kept in .env file
+//it works on sandbox enpoint. Sandbox mode available on their website
+// begin - rescue - end #=> designed to rescue an app from an error that comes up.
+// it is here to make sure, that it returns nil when client doesnt return any result
+// and therefore gives alert, that invalid symbol has been entered in stocks_controller
