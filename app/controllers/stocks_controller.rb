78c8bd0cb9683f89fcf0206b0f6201c16e4051bc
Artur Mrozinski <amrozinski@gmail.com>
2021-01-03 13:53:03 +0000

    finished implementing JS into Rails

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present? # has it not been left blank??
       @stock = Stock.new_lookup(params[:stock])
         if @stock # if stock.rb did not return a nil from rescue action
-          render 'users/my_portfolio'
+          respond_to do |format|
+            format.js { render partial: 'users/result' }
+          end
         else
-          flash[:alert] = "Please enter a valid symbol to search"
-          redirect_to my_portfolio_path
+          respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+          end
         end
     else
-        flash[:alert] = "This field canot be left blank"
-        redirect_to my_portfolio_path
+      respond_to do |format|
+      flash.now[:alert] = "This field canot be left blank"
+      format.js { render partial: 'users/result' }
+      end
     end
   end
 end
